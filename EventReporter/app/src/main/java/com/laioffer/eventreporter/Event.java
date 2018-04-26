d995e846e92bd032b2373cc78107ada146c4ffd0
qxzhou <zhou40469269@gmail.com>
2018-04-25 20:26:27 -0400

    firstUpload

---
+++ EventReporter/app/src/main/java/com/laioffer/eventreporter/Event.java
@@ -0,0 +1,123 @@
+package com.laioffer.eventreporter;
+
+/**
+ * Created by qianxi on 3/4/18.
+ */
+
+public class Event {
+
+    /**
+     * All data for a event.
+     */
+    private String title;
+    private String address;
+    private String description;
+    private int like;
+    private String id;
+    private long time;
+    private String username;
+    private String imgUri;
+    private int CommentNumber;
+    private double latitude;
+    private double longitude;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Event() {}
+
+    /**
+     * Constructor
+     */
+    public Event(String title, String address, String description) {
+        this.title = title;
+        this.address = address;
+        this.description = description;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+
+    public int getCommentNumber() {
+        return CommentNumber;
+    }
+
+    public void setCommentNumber(int commentNumber) {
+        CommentNumber = commentNumber;
+    }
+}
